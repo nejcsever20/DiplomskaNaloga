f825408b2b6dd1e188a7af5cb761788e99ab139c
Nejc Sever Mihelič <nejc.severmihelic@gorenje.com>
2025-05-19 12:39:59 +0200

    git ignored some files

--- obj/Debug/net8.0/diplomska.AssemblyInfo.cs
+++ obj/Debug/net8.0/diplomska.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24e877fc68018a9ec1fc10194f09e166ae8f5b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604fb31bca037238d5c58a04636ec3c46005e929")]
 [assembly: System.Reflection.AssemblyProductAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
