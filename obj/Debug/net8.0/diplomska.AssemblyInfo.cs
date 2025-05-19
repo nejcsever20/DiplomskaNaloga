6a4ba94bd6451e4512ee9dddf3ec580a48044061
Nejc Sever Mihelič <nejc.severmihelic@gorenje.com>
2025-05-19 08:15:38 +0200

    fixed the issues

--- obj/Debug/net8.0/diplomska.AssemblyInfo.cs
+++ obj/Debug/net8.0/diplomska.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d69dd1162abc99cb6184add50065370b6f7c650")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0922ac06d0e9de86c7c8c4df3084beb27aaa9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
