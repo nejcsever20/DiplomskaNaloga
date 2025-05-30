c979b681267ca06bec4b02ddd49e993e37169e3d
Nejc Sever Mihelič <nejc.severmihelic@gorenje.com>
2025-05-30 11:58:34 +0200

    Testing to see if there are any updates

--- obj/Debug/net8.0/diplomska.AssemblyInfo.cs
+++ obj/Debug/net8.0/diplomska.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48dfb6e25638cdde3e2456ea5083874db6657f94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6c7fa6b6150419829cab1fa67a8a5999ff727a")]
 [assembly: System.Reflection.AssemblyProductAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
