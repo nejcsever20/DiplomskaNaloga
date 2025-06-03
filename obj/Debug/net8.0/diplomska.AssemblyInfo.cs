17d85180f1132a6dfcb5a9f3dfb447c632535705
Nejc Sever Mihelič <nejc.severmihelic@gorenje.com>
2025-06-03 11:20:36 +0200

    Added file importing as excel, csv, txt

--- obj/Debug/net8.0/diplomska.AssemblyInfo.cs
+++ obj/Debug/net8.0/diplomska.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6c7fa6b6150419829cab1fa67a8a5999ff727a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c979b681267ca06bec4b02ddd49e993e37169e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
