a6f2fe571343d3a53b171e3e3c73575317603d29
Nejc Sever Mihelič <nejc.severmihelic@gorenje.com>
2025-06-10 09:38:36 +0200

    Added a callback and archive compare transports

---
+++ Pages/Admin/ArchivedTransports.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using diplomska.Data;
+using diplomska.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace diplomska.Pages.Admin
+{
+    public class ArchivedTransportsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArchivedTransportsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArchivedTransport> ArchivedTransports { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            ArchivedTransports = await _context.ArchivedTransports
+                .OrderByDescending(t => t.PlaniranPrihod)
+                .ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostRemoveArchiveAsync(long id)
+        {
+            var archived = await _context.ArchivedTransports.FindAsync(id);
+
+            if (archived == null)
+            {
+                ModelState.AddModelError(string.Empty, "Archived transport not found");
+                return Page();
+            }
+
+            _context.ArchivedTransports.Remove(archived);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
+        }
+    }
+}
--- obj/Debug/net8.0/diplomska.AssemblyInfo.cs
+++ obj/Debug/net8.0/diplomska.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b127cad9c6c9fd035a1914a6a1119b9dac8bd389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f60fdbfcd4cf8f4dd6a776d712d358fac8cf507a")]
 [assembly: System.Reflection.AssemblyProductAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diplomska")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
